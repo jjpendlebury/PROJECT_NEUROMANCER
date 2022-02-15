fcaaeb3065a8c480c28f4e0d01f85f670931dfc6
jjpendlebury <61416766+jjpendlebury@users.noreply.github.com>
2022-02-15 13:31:15 +0000

    Matrix Mult 1
    code currently in main

--- Software/Project Neuromancer/source/Project_Neuromancer.cpp
+++ Software/Project Neuromancer/source/Project_Neuromancer.cpp
@@ -12,17 +12,64 @@ int COLUMN_COUNT = 5;
 
 //2d vector initialisaiton with a default value
 std::vector<std::vector<float> > fog(ROW_COUNT, std::vector<float>(COLUMN_COUNT, 4));
-vector< vector<float> > bar = { { 1, 1, 1 }, { 2, 2, 2 } };  
+vector< vector<float> > bar = { { 1, 1 }, {2, 2}    };  
+vector< vector<float> > zog = { { 3, 3 }, {4,4}     };
 
-int main()
-{
-    //this also works post-init
-    bar = { { 1, 1, 1 }, { 2, 2, 2 } };
-    cout << "Hello World!\n";
-    test_matrix.set_data(fog);
-    test_matrix.disp_data();
-    test_matrix2.set_data(bar);
-    test_matrix2.disp_data();
+//int main()
+//{
+//    //this also works post-init
+//    bar = { { 1, 1, 1 }, { 2, 2, 2 } };
+//    cout << "Hello World!\n";
+//    test_matrix.set_data(fog);
+//    test_matrix.disp_data();
+//    test_matrix2.set_data(bar);
+//    test_matrix2.disp_data();
+//}
+
+int main() {
+    int product[10][10], r1 = 3, c1 = 2, r2 = 2, c2 = 3, i, j, k;
+    vector< vector<int> > a = { {2, 4, 6} , {2, 3, 5} , {3, 1, 4} };
+    vector< vector<int> > b = { {1, 2, 3} , {3, 6, 1}, {3, 1, 4} };
+    r1 = a.size();
+    r2 = b.size();
+    c1 = a[0].size();
+    c2 = b[0].size();
+
+    if (c1 != r2) {
+        cout << "Column of first matrix should be equal to row of second matrix";
+    }
+    else {
+        cout << "The first matrix is:" << endl;
+        for (i = 0; i < r1; ++i) {
+            for (j = 0; j < c1; ++j)
+                cout << a[i][j] << " ";
+            cout << endl;
+        }
+        cout << endl;
+        cout << "The second matrix is:" << endl;
+        for (i = 0; i < r2; ++i) {
+            for (j = 0; j < c2; ++j)
+                cout << b[i][j] << " ";
+            cout << endl;
+        }
+        cout << endl;
+        for (i = 0; i < r1; ++i)
+            for (j = 0; j < c2; ++j) {
+                product[i][j] = 0;
+            }
+        for (i = 0; i < r1; ++i)
+            for (j = 0; j < c2; ++j)
+                for (k = 0; k < c1; ++k) {
+                    product[i][j] += a[i][k] * b[k][j];
+                }
+        cout << "Product of the two matrices is:" << endl;
+        for (i = 0; i < r1; ++i) {
+            for (j = 0; j < c2; ++j)
+                cout << product[i][j] << " ";
+            cout << endl;
+        }
+    }
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
