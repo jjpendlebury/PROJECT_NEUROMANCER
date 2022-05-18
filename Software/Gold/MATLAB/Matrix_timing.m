1884bda944a502b3d578d376918455f3b5bb89f4
Jack Pendlebury <jack.j.pendlebury@students.plymouth.ac.uk>
2022-05-18 16:42:38 +0100

    Matlab timing done

--- Software/Gold/MATLAB/Matrix_timing.m
+++ Software/Gold/MATLAB/Matrix_timing.m
@@ -1,6 +1,24 @@
 close all
 clear all
 clc
+max_dims = 20;
 
-test = zeros(10,10)
-test2 = zeros(10,10)
+output_mat = zeros(0,0);
+output_row = zeros(0,0);
+for k = 1:20
+    n=10;
+    test = reshape(1:(k*k), k, k) .';
+    test2 = reshape(1:(k*k), k, k) .';
+    output_row = k;
+    for m = 1:3
+    tic;
+    for i = 1:10000
+        test * test2;
+    end
+    output_row = [output_row toc];
+    end
+    output_mat= [output_mat;output_row mean(output_row(2:4))];
+    
+end
+
+writematrix(output_mat, 'matlab_timing_output.csv')
